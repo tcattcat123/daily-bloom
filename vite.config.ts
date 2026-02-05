76114021284533fe65719b10782711c1a013b9e5
tcattcat123 <terakot2025@gmail.com>
2026-02-06 00:05:44 +0800

    style: optimize PWA icons for maskable support

--- vite.config.ts
+++ vite.config.ts
@@ -29,12 +29,14 @@ export default defineConfig(({ mode }) => ({
           {
             src: 'favicon.png',
             sizes: '192x192',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
           },
           {
             src: 'favicon.png',
             sizes: '512x512',
-            type: 'image/png'
+            type: 'image/png',
+            purpose: 'any maskable'
           }
         ]
       }
